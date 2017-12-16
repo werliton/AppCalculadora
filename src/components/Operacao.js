df40780a3f00bb7b7a896a879fb2cc9bdae00b64
werliton <werlitoncarlos@gmail.com>
2017-12-16 19:35:59 +0000

    Alterando o componente operacao

--- src/components/Operacao.js
+++ src/components/Operacao.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Picker ,StyleSheet } from 'react-native';
 
-export default props => (
-    <Picker style={styles.operacao}>
-      <Picker.Item label='Soma' value='soma'/>
-      <Picker.Item label='Subtração' value='subracao'/>
-    </Picker>
-);
+export default class Operacao extends Component{
+	constructor(props){
+		super(props);
+
+		this.state = { operacao: '' };
+	}
+
+
+	render(){
+		return (
+			<Picker 
+		    	style={styles.operacao}
+		    	selectedValue={this.state.operacao}
+		    	onValueChange={ operation => this.setState({ operacao: operation })}
+		    	>
+		      <Picker.Item label='Soma' value='soma'/>
+		      <Picker.Item label='Subtração' value='subracao'/>
+		    </Picker>
+		);
+	}
+} 
 
 const styles = StyleSheet.create({
   operacao:{
